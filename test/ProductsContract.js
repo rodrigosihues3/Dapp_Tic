71a0baa526578b47baed98812283675705be9941
rodrigosihues3 <u24254406@utp.edu.pe>
2024-12-10 03:43:52 -0500

    Test unitarios implementados

--- test/ProductsContract.js
+++ test/ProductsContract.js
@@ -20,18 +20,24 @@ contract("ProductsContract", () => {
         assert.equal(product.id.toNumber(), productsCounter);
         assert.equal(product.title, "Producto de ejemplo");
         assert.equal(product.description, "Ejemplo de descripcion del producto");
+        assert.equal(product.expirationDate, "Fecha de ejemplo");
+        assert.equal(product.brand, "Marca de ejemplo");
+        assert.equal(product.lote, "Lote de ejemplo");
         assert.equal(product.published, false);
         assert.equal(productsCounter, 1);
     })
 
     it('Producto creado con exito.', async () => {
-        const result = await this.productsContract.createProduct("Test titulo de producto", "Test decripcion de producto");
+        const result = await this.productsContract.createProduct("Test titulo de producto", "Test decripcion de producto", "Test de fecha de producto", "Test de marca de producto", "Test de lote de producto");
         const productEvent = result.logs[0].args;
         const productsCounter = await this.productsContract.productsCounter();
 
         assert.equal(productEvent.id.toNumber(), 2);
         assert.equal(productEvent.title, "Test titulo de producto");
         assert.equal(productEvent.description, "Test decripcion de producto");
+        assert.equal(productEvent.expirationDate, "Test de fecha de producto");
+        assert.equal(productEvent.brand, "Test de marca de producto");
+        assert.equal(productEvent.lote, "Test de lote de producto");
         assert.equal(productEvent.published, false);
         assert.equal(productsCounter, 2);
     })    
@@ -47,13 +53,17 @@ contract("ProductsContract", () => {
     })
 
     it('Modificar producto.', async () => {
-        const result = await this.productsContract.updateProduct(0, "Titulo cambiado", "Decripcion cambiada");
+        const result = await this.productsContract.updateProduct(0, "Titulo cambiado", "Decripcion cambiada", "Fecha cambiada", "Marca cambiada", "Lote cambiado", "Razón agregada");
         const productEvent = result.logs[0].args;
         const product = await this.productsContract.products(0);
         
         assert.equal(productEvent.id.toNumber(), 0);
         assert.equal(product.title, "Titulo cambiado");
         assert.equal(product.description, "Decripcion cambiada");
+        assert.equal(product.expirationDate, "Fecha cambiada");
+        assert.equal(product.brand, "Marca cambiada");
+        assert.equal(product.lote, "Lote cambiado");
+        assert.equal(product.reasonForChange, "Razón agregada");
     })
 
     it('Eliminar datos de un producto.', async () => {
@@ -64,5 +74,9 @@ contract("ProductsContract", () => {
         assert.equal(product.id.toNumber(), 1);
         assert.equal(product.title, "");
         assert.equal(product.description, "");
+        assert.equal(product.expirationDate, "");
+        assert.equal(product.brand, "");
+        assert.equal(product.lote, "");
+        assert.equal(product.reasonForChange, "");
     })
 })
